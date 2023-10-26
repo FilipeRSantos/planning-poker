370d160bd82df1a5eb498981d67cec2f0985f138
Filipe Santos <fprssantos@gmail.com>
2023-10-25 23:07:22 -0300

    feat: added htmx and first integration

--- adapters/RedisClient.go
+++ adapters/RedisClient.go
@@ -2,11 +2,17 @@ package adapters
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 	"os"
 )
 
 var client *redis.Client
+
+type UserDetails struct {
+	user            string
+	currentEstimate int
+}
 
 func CreateClient() (*redis.Client, error) {
 	opt, err := redis.ParseURL(os.Getenv("REDIS_CONNECTION_STRING"))
@@ -20,4 +26,16 @@ func CreateClient() (*redis.Client, error) {
 
 func IncAndGetRefreshed(ctx context.Context) (int64, error) {
 	return client.Incr(ctx, "refreshes").Result()
+}
+
+func NewRoom(ctx context.Context, user string) string {
+	uid := uuid.New().String()
+
+	//Store in a set a new room
+	client.SAdd(ctx, "rooms", uid)
+
+	//Add the current user to the newly created room in a hash
+	client.HSet(ctx, uid, UserDetails{user: user, currentEstimate: 0})
+
+	return uid
 }
--- endpoints/api-endpoints.go
+++ endpoints/api-endpoints.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"github.com/gin-gonic/gin"
+	"liposo/planing-poker/adapters"
 	"net/http"
 )
 
@@ -9,8 +10,21 @@ func SetupApiEndpoints(router *gin.Engine) {
 	v1 := router.Group("/api")
 
 	v1.GET("/healthz", handleHealthCheck)
+
+	roomRoutes := v1.Group("/room")
+	roomRoutes.POST("/", handleNewRoom)
 }
 
 func handleHealthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, nil)
+}
+
+func handleNewRoom(c *gin.Context) {
+	userName := c.PostForm("user")
+	newRoom := adapters.NewRoom(c.Request.Context(), userName)
+
+	c.HTML(http.StatusCreated, "room-created.html", gin.H{
+		"uid":  newRoom,
+		"user": userName,
+	})
 }
